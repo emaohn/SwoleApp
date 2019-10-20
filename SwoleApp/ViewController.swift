dfed0ecbd2a3af3fcd5ba112b21d7946b302b7ee
Emmie Ohnuki <emaohn@gmail.com>
2019-10-19 23:08:48 -0400

    Initial Commit

---
+++ SwoleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwoleApp
+//
+//  Created by Emmie Ohnuki on 10/19/19.
+//  Copyright © 2019 Emmie Ohnuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
