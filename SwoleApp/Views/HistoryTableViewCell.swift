255c9c6f4eb4b9447474b353360cf8f44bd87585
Emmie Ohnuki <emaohn@gmail.com>
2019-10-19 23:17:10 -0400

    initial setup

---
+++ SwoleApp/Views/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  SwoleApp
+//
+//  Created by Emmie Ohnuki on 10/19/19.
+//  Copyright © 2019 Emmie Ohnuki. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
